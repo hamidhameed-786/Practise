bfd0a5e67f19517a98933f3ea1ff1442ee0deef2
hamid <hamidhameed591@gmail.com>
2025-10-16 09:13:45 -0700

    practise do

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,9 +1,26 @@
-import Callback from "./Callback/Callback.jsx";
-function App(){
-    return(
-        <>
-        <Callback />
-        </>
-    );
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Cs/Header";
+import Footer from "./Cs/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Blog from "./Pages/Blog";
+import Services from "./Pages/Services";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
+
 export default App;
